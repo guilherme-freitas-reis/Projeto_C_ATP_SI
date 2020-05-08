adb410151c76f2ff0ec0f0d9ad170b6ec686f3d5
GuilhermeReis9876 <guilhermereis9876@gmail.com>
2020-05-07 22:29:21 -0300

    Inclusão da função Guilherme Reis

--- AtpSI.c
+++ AtpSI.c
@@ -1,92 +1,125 @@
 #include <stdio.h>
 #include <locale.h>
 
-void pedro();
-void victorPaizante();
+void guilhermeFreitasReis();
 void igorFloresPatrocinio();
-void victorJavierFerreiraSilva();
 void igorJackson();
 void marcos();
+void pedro();
+void victorPaizante();
+void victorJavierFerreiraSilva();
 void vitorLacerda();
 
-main(){
+main()
+{
 	setlocale(LC_ALL, "Portuguese");
 	float numeros[10];
 	int opcaoMenu;
 	printf("Digite 10 números separados por espaço para iniciar: ");
 	scanf("%f %f %f %f %f %f %f %f %f %f", &numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4], &numeros[5], &numeros[6], &numeros[7], &numeros[8], &numeros[9]);
 
-	while(1){
+	while (1)
+	{
 		printf("\n\nDigite um número de 1 a 18 para selecionar uma função, ou digite 0 para alterar os números: ");
 		scanf("%d", &opcaoMenu);
-		
-		switch(opcaoMenu){
-			case 0:
-				printf("\n\nDigite 10 números separados por espaço: ");
-				scanf("%f %f %f %f %f %f %f %f %f %f", &numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4], &numeros[5], &numeros[6], &numeros[7], &numeros[8], &numeros[9]);
-				break;		
-			case 10:
-				igorFloresPatrocinio();
-        break;
-			case 11:
-				igorJackson();
-				break;
-			case 13:
-				marcos();
-				break;
-      case 14:
-				pedro();
-				break;
-      case 16:
-				victorPaizante();
-        break;
-      case 17:
-				victorJavierFerreiraSilva();
-				break;
-			case 18:
-				vitorLacerda();
-				break;
-			default:
-				printf("\n\nOpção inválida");
-				break;
+
+		switch (opcaoMenu)
+		{
+		case 0:
+			printf("\n\nDigite 10 números separados por espaço: ");
+			scanf("%f %f %f %f %f %f %f %f %f %f", &numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4], &numeros[5], &numeros[6], &numeros[7], &numeros[8], &numeros[9]);
+			break;
+		case 6:
+			guilhermeFreitasReis();
+			break;
+		case 10:
+			igorFloresPatrocinio();
+			break;
+		case 11:
+			igorJackson();
+			break;
+		case 13:
+			marcos();
+			break;
+		case 14:
+			pedro();
+			break;
+		case 16:
+			victorPaizante();
+			break;
+		case 17:
+			victorJavierFerreiraSilva();
+			break;
+		case 18:
+			vitorLacerda();
+			break;
+		default:
+			printf("\n\nOpção inválida");
+			break;
 		}
 	}
 }
 
-void pedro(){
-	printf("Pedro Henrique Bernardes Ribeiro\n");
+void guilhermeFreitasReis()
+{
+	printf("\nGuilherme de Freitas Reis\n");
+	printf("\nDigite qualque tecla para continuar.\n");
 	fflush(stdin);
 	getchar();
 }
 
-void victorPaizante(){
-  printf("\nVictor Paizante");
-}
-
-void igorFloresPatrocinio(){
-	printf("Igor Flores Patroc�nio");
-}
-
-void victorJavierFerreiraSilva(){
-	printf("Victor Javier Ferreira e Silva");
-}
-
-void igorJackson(){
-	printf("\nIgor Jackson Leal Bovolenta");
-	printf("\nDigite qualquer tecla para continuar.");
+void igorFloresPatrocinio()
+{
+	printf("\nIgor Flores Patroc�nio\n");
+	printf("\nDigite qualque tecla para continuar.\n");
 	fflush(stdin);
 	getchar();
 }
 
-void marcos(){
+void igorJackson()
+{
+	printf("\nIgor Jackson Leal Bovolenta\n");
+	printf("\nDigite qualquer tecla para continuar.\n");
+	fflush(stdin);
+	getchar();
+}
+
+void marcos()
+{
 	printf("\n\t------------ Marcos Vinicius de Sousa Pontes ------------\n");
 	printf("\nDigite qualque tecla para continuar.\n");
 	fflush(stdin);
 	getchar();
 }
 
-void vitorLacerda(){
-  printf("vitor Lacerda\n");
-  fflush(stdin);
-  getchar();
+void pedro()
+{
+	printf("\nPedro Henrique Bernardes Ribeiro\n");
+	printf("\nDigite qualque tecla para continuar.\n");
+	fflush(stdin);
+	getchar();
+}
+
+void victorPaizante()
+{
+	printf("\nVictor Paizante\n");
+	printf("\nDigite qualque tecla para continuar.\n");
+	fflush(stdin);
+	getchar();
+}
+
+void victorJavierFerreiraSilva()
+{
+	printf("\nVictor Javier Ferreira e Silva\n");
+	printf("\nDigite qualque tecla para continuar.\n");
+	fflush(stdin);
+	getchar();
+}
+
+void vitorLacerda()
+{
+	printf("\nVitor Lacerda\n");
+	printf("\nDigite qualque tecla para continuar.\n");
+	fflush(stdin);
+	getchar();
 }
