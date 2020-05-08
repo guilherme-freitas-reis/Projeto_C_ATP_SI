7b9fb8af558dcd05db77127b9d195d1a7114ab89
Pedro <pedrohbribeiro42@gmail.com>
2020-05-08 19:40:15 -0300

    Correções gerais no código
    Correção da identação do switch case
    Comando de Digite qualquer tecla para continuar adicionado após o switch case e removido das funções individuais, de forma a evitar código duplicado
    Correções de acentuação

--- AtpSI.c
+++ AtpSI.c
@@ -16,99 +16,85 @@ main()
 	setlocale(LC_ALL, "Portuguese");
 	float numeros[10];
 	int opcaoMenu;
-	printf("Digite 10 números separados por espaço para iniciar: ");
+	printf("Digite 10 n�meros separados por espa�o para iniciar: ");
 	scanf("%f %f %f %f %f %f %f %f %f %f", &numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4], &numeros[5], &numeros[6], &numeros[7], &numeros[8], &numeros[9]);
 
 	while (1)
 	{
-		printf("\n\nDigite um número de 1 a 18 para selecionar uma função, ou digite 0 para alterar os números: ");
+		printf("\n\nDigite um n�mero de 1 a 18 para selecionar uma fun��o, ou digite 0 para alterar os n�meros: ");
 		scanf("%d", &opcaoMenu);
-
+				
 		switch (opcaoMenu)
 		{
-		case 0:
-			printf("\n\nDigite 10 números separados por espaço: ");
-			scanf("%f %f %f %f %f %f %f %f %f %f", &numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4], &numeros[5], &numeros[6], &numeros[7], &numeros[8], &numeros[9]);
-			break;
-		case 6:
-			guilhermeFreitasReis();
-			break;
-		case 8:
-		     gustavoMeirelesSousa();
-			 break;	
-		case 10:
-			igorFloresPatrocinio();
-			break;
-		case 11:
-			igorJackson();
-			break;
-		case 13:
-			marcos();
-			break;
-		case 14:
-			pedro();
-			break;
-		case 16:
-			victorPaizante();
-			break;
-		case 17:
-			victorJavierFerreiraSilva();
-			break;
-		case 18:
-			vitorLacerda();
-			break;
-		default:
-			printf("\n\nOpção inválida");
-			break;
+			case 0:
+				printf("\n\nDigite 10 n�meros separados por espa�o: ");
+				scanf("%f %f %f %f %f %f %f %f %f %f", &numeros[0], &numeros[1], &numeros[2], &numeros[3], &numeros[4], &numeros[5], &numeros[6], &numeros[7], &numeros[8], &numeros[9]);
+				break;
+			case 6:
+				guilhermeFreitasReis();
+				break;
+			case 8:
+			    gustavoMeirelesSousa();
+				break;	
+			case 10:
+				igorFloresPatrocinio();
+				break;
+			case 11:
+				igorJackson();
+				break;
+			case 13:
+				marcos();
+				break;
+			case 14:
+				pedro();
+				break;
+			case 16:
+				victorPaizante();
+				break;
+			case 17:
+				victorJavierFerreiraSilva();
+				break;
+			case 18:
+				vitorLacerda();
+				break;
+			default:
+				printf("\n\nOp��o inv�lida");
+				break;
 		}
+		
+		printf("\nDigite qualque tecla para continuar.\n");
+		fflush(stdin);
+		getchar();
 	}
 }
 
 void guilhermeFreitasReis()
 {
 	printf("\nGuilherme de Freitas Reis\n");
-	printf("\nDigite qualque tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
 void gustavoMeirelesSousa()
 {
 	printf("\nGustavo Meireles Sousa\n");
-	printf("\nDigite qualquer tecla para continuar\n");
-	fflush(stdin);
-	getchar();
 
 }
 void igorFloresPatrocinio()
 {
 	printf("\nIgor Flores Patroc�nio\n");
-	printf("\nDigite qualque tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
 
 void igorJackson()
 {
 	printf("\nIgor Jackson Leal Bovolenta\n");
-	printf("\nDigite qualquer tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
 
 void marcos()
 {
 	printf("\n\t------------ Marcos Vinicius de Sousa Pontes ------------\n");
-	printf("\nDigite qualque tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
 
 void pedro()
 {
 	printf("\nPedro Henrique Bernardes Ribeiro\n");
-	printf("\nDigite qualque tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
 
 void victorPaizante()
@@ -122,15 +108,9 @@ void victorPaizante()
 void victorJavierFerreiraSilva()
 {
 	printf("\nVictor Javier Ferreira e Silva\n");
-	printf("\nDigite qualque tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
 
 void vitorLacerda()
 {
 	printf("\nVitor Lacerda\n");
-	printf("\nDigite qualque tecla para continuar.\n");
-	fflush(stdin);
-	getchar();
 }
